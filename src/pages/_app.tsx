bdd388ee9f90d9fe4fb6604cce0eca2b456656a5
Kamil M <kamil.w.maron@gmail.com>
2023-02-28 12:57:59 +0100

    Added StyledComponents

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 import store from "../store";
+import { GlobalStyles } from "../GlobalStyles";
 
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
+
+export default MyApp;
