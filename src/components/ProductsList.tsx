ee8393d16b934047dcd1421ef07026b52a289b08
Kamil M <kamil.w.maron@gmail.com>
2023-02-28 13:17:00 +0100

    Added search and filter

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -12,6 +12,28 @@ const [selectedProduct, setSelectedProduct] = useState<{ id: number; name: strin
 const [showAddForm, setShowAddForm] = useState(false);
 const [showEditForm, setShowEditForm] = useState(false);
 const [showDeleteForm, setShowDeleteForm] = useState(false);
+const [searchTerm, setSearchTerm] = useState("");
+const [filterOptions, setFilterOptions] = useState({
+  minPrice: "",
+  maxPrice: "",
+});
+
+const filteredProducts = useMemo(() => {
+    let filtered = products;
+    if (searchTerm) {
+      filtered = filtered.filter((p) =>
+        p.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+    if (filterOptions.minPrice) {
+      filtered = filtered.filter((p) => p.price >= Number(filterOptions.minPrice));
+    }
+    if (filterOptions.maxPrice) {
+      filtered = filtered.filter((p) => p.price <= Number(filterOptions.maxPrice));
+    }
+    return filtered;
+  }, [products, searchTerm, filterOptions]);
+  
 
 const handleAddProduct = () => {
 setSelectedProduct(null);
@@ -36,18 +58,56 @@ setShowDeleteForm(false);
 
 return (
 <div>
+<div>
+  <input
+    type="text"
+    placeholder="Search by name"
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)}
+  />
+  <select
+    value={filterOptions.minPrice}
+    onChange={(e) =>
+      setFilterOptions({
+        ...filterOptions,
+        minPrice: e.target.value,
+      })
+    }
+  >
+    <option value="">Min price</option>
+    <option value="10">$10</option>
+    <option value="20">$20</option>
+    <option value="50">$50</option>
+  </select>
+  <select
+    value={filterOptions.maxPrice}
+    onChange={(e) =>
+      setFilterOptions({
+        ...filterOptions,
+        maxPrice: e.target.value,
+      })
+    }
+  >
+    <option value="">Max price</option>
+    <option value="50">$50</option>
+    <option value="100">$100</option>
+    <option value="200">$200</option>
+  </select>
+</div>
+
 {isLoading && <div>Loading...</div>}
 {error && <div>{error}</div>}
 <button onClick={handleAddProduct}>Add Product</button>
 <ul>
-{products.map((product) => (
-<li key={product.id}>
-{product.name} ({product.price})
-<button onClick={() => handleEditProduct(product)}>Edit</button>
-<button onClick={() => handleDeleteProduct(product)}>Delete</button>
-</li>
-))}
+  {filteredProducts.map((product) => (
+    <li key={product.id}>
+      {product.name} ({product.price})
+      <button onClick={() => handleEditProduct(product)}>Edit</button>
+      <button onClick={() => handleDeleteProduct(product)}>Delete</button>
+    </li>
+  ))}
 </ul>
+
 {showAddForm && <PopupForm mode="add" onClose={handleCloseForms} />}
 {showEditForm && selectedProduct && <PopupForm mode="edit" product={selectedProduct} onClose={handleCloseForms} />}
 {showDeleteForm && selectedProduct && (
